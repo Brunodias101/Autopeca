35936ca9d949ec8d90c17d4207e693a1275f66d7
brunodias101 <Br@22356uno>
2022-12-30 17:22:35 -0300

    primeiro commit

---
+++ src/main/java/com/autopeca/autopeca/AutopecaApplication.java
@@ -0,0 +1,13 @@
+package com.autopeca.autopeca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutopecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutopecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autopeca/autopeca/AutopecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autopeca.autopeca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutopecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
